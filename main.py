d57d42ea716897f0c76cb486b3299b366208f1db
SatanicTriangle <satanictriangle@ubuntu.dev>
2020-03-23 20:10:54 +0100

    more functional script

--- main.py
+++ main.py
@@ -1,51 +1,74 @@
 from os import listdir
 from os.path import isfile, join
 import re
+def findValueTypes(path):
+    valuetypeslist = []
+    regexp1 = re.compile(r"[A-Z,a-z,',',\s,0-9]*\:\s*[A-Z,a-z,',',\s]*\.")
+    onlyfiles = [f for f in listdir(path) if isfile(join(path, f))]
+    for fileName in onlyfiles:
+        if ".names" in fileName:
+            with open(path + fileName, "r") as file:
+                count = 0
+                countAdded = 0
+                for line in file:
+                    expline = regexp1.search(line)
+                    if expline: 
+                        count +=1
+                        expline = expline.string[0:expline.string.find(":")]
+                        expline = expline.replace(' ','_')
+                        if expline not in valuetypeslist:
+                            valuetypeslist.append(expline)
+                            countAdded += 1
+            print("File: \033[91m" + fileName + "\033[39m contains \033[92m" + str(count) + "\033[39m value types including \033[92m" + str(countAdded) + "\033[39m new.")
+    return valuetypeslist
 
-typeslist = []
-classlist = []
-classcount = []
-regexp1 = re.compile(r"[A-Z,a-z,',',\s,0-9]*\:\s*[A-Z,a-z,',',\s]*\.")
-regexp2 = re.compile(r"[A-Z,a-z,',',\s,0-9]*\:")
-mypath = "thyroid-disease/"
-onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
-for fileName in onlyfiles:
-    if ".names" in fileName:
-        print (fileName)
-        with open(mypath + fileName, "r") as file:
-            count = 0
-            for line in file:
-                expline = regexp1.search(line)
-                if expline: 
-                    count +=1
-                    expline = expline.string[0:expline.string.find(":")]
-                    expline = expline.replace(' ','_')
-                    if expline not in typeslist:
-                        typeslist.append(expline)
-                else:
-                    if ';' not in line:
+def findClasses(path):
+    classlist = []
+    regexp1 = re.compile(r"[A-Z,a-z,',',\s,0-9]*\:\s*[A-Z,a-z,',',\s]*\.")
+    onlyfiles = [f for f in listdir(path) if isfile(join(path, f))]
+    for fileName in onlyfiles:
+        if ".names" in fileName:
+            with open(path + fileName, "r") as file:
+                count = 0
+                countAdded = 0
+                for line in file:
+                    test = regexp1.search(line)
+                    if not test and ';' not in line: 
                         line = line.strip()
                         if '.' in line:
                             line = line[:line.index('.')]
                         for Class in line.split(','):
-                            if Class not in classlist:
-                                classlist.append(Class.strip())
-                                classcount.append(0)
-            print (count)
-classlist.remove('')
-for fileName in onlyfiles:
-    if ".data" in fileName:
-        print (fileName)
-        with open(mypath + fileName, "r") as file:
-            for line in file:
-                for i in range(len(classlist)):
-                    if classlist[i] in line:
-                        classcount[i] += 1
-print("-------------")
-for item in typeslist:
-    print(item)
-print(len(typeslist))
+                            Class = Class.strip()
+                            if  len(Class) != 0:
+                                count += 1
+                                if Class not in classlist:
+                                    countAdded += 1
+                                    classlist.append(Class)
+            print("File: \033[91m" + fileName + "\033[39m contains \033[92m" + str(count) + "\033[39m classes including \033[92m" + str(countAdded) + "\033[39m new.")
+    return classlist
 
-print("-------------")   
-for i in range(len(classlist)):
-    print (classlist[i] + " - " + str(classcount[i]))
+
+def countClasses(path, classlist, all = False):
+    classcount = [0] * len(classlist) #apparently this is completely normal code in python
+    onlyfiles = [f for f in listdir(path) if isfile(join(path, f))]
+    for fileName in onlyfiles:
+        if ".data" in fileName:
+            with open(path + fileName, "r") as file:
+                currentfilelasscount = [0] * len(classlist)
+                for line in file:
+                    for i in range(len(classlist)):
+                        if classlist[i] in line:
+                            classcount[i] += 1
+                            currentfilelasscount[i] += 1
+            print ("File \033[91m" + fileName + "\033[39m contains:")
+            for i in range(len(classlist)):
+                if currentfilelasscount[i] > 0 or all:
+                    print ("\t" + classlist[i] + " - \033[92m" + str(currentfilelasscount[i]) + "\033[39m")
+    return classcount
+
+dir = "thyroid-disease/"
+values = findValueTypes(dir)
+print("-----------------------------------------------------------")
+classes = findClasses(dir)
+print("-----------------------------------------------------------")
+classcount = countClasses(dir, classes)
