8cd3b0849bdd58aa0c75b6eb732b9bf68c5ad0dd
foralost <kombii4@gmail.com>
2020-04-07 19:05:18 +0200

    Second attempt tenso

--- tenso/src.py
+++ tenso/src.py
@@ -1,41 +1,12 @@
-#!/bin/python
-import tensorflow as tf
-import pandas as pd
+import functools
+
 import numpy as np
+import tensorflow as tf
 
+import pandas as pd
 
-# dane pacjenta
-class PackNumericFeatures(object):
-    def __init__(self, names):
-        self.names = names
-
-    def __call__(self, features, labels):
-        numeric_features = [features.pop(name) for name in self.names]
-        numeric_features = [tf.cast(feat, tf.float32) for feat in numeric_features]
-        numeric_features = tf.stack(numeric_features, axis=-1)
-        features['numeric'] = numeric_features
-
-        return features, labels
-
-
-def retClassLabels(iClassCount):
-    toRet = []
-    iNumb = 0
-    while iNumb <= iClassCount ** 2:
-        toRet.append("{0:b}".format(iNumb).zfill(iClassCount))
-        iNumb += 1
-    return toRet
-
-
-def normalize_numeric_data(data, mean, std):
-    # Center the data
-    return (data - mean) / std
-
-
-def show_batch(dataset):
-    for batch, label in dataset.take(1):
-        for key, value in batch.items():
-            print("{:20s}: {}".format(key, value.numpy()))
+# labal value to predict
+LABEL_COLUMN = 'flags'
 
 
 def getHeader(src):
@@ -45,14 +16,22 @@ def getHeader(src):
 
     toRet = header.split(',')
     toRet[-1] = toRet[-1][:-1]  # remove \n
-    return toRet
+    return
+
+
+def show_batch(dataset):
+    print( type(dataset.take(1)) )
+    for batch, label in dataset.take(1):
+        print(type(batch.items()))
+        for key, value in batch.items():
+            print("{:20s}: {}".format(key, value.numpy()))
 
 
 def get_dataset(file_path, **kwargs):
     dataset = tf.data.experimental.make_csv_dataset(
         file_path,
-        batch_size=5,  # Artificially small to make examples easier to show.
-        label_name='flags',
+        batch_size=10,  # Artificially small to make examples easier to show.
+        label_name=LABEL_COLUMN,
         na_value="?",
         num_epochs=1,
         ignore_errors=True,
@@ -61,53 +40,155 @@ def get_dataset(file_path, **kwargs):
 
 
 def pack(features, label):
+    print(features.values(), " ", label, '\n')
     return tf.stack(list(features.values()), axis=-1), label
 
 
-header_num = ['age', 'TSH', 'T3', 'TT4']
+class PackNumericFeatures(object):
+    def __init__(self, names):
+        self.names = names
+
+    def __call__(self, features, labels):
+        numeric_features = [features.pop(name) for name in self.names]
+        numeric_features = [tf.cast(feat, tf.float32) for feat in numeric_features]
+        numeric_features = tf.stack(numeric_features, axis=-1)
+        features['numeric'] = numeric_features
+        return features, labels
+
+
+def normalize_numeric_data(data, mean, std):
+    # Center the data
+    return (data - mean) / std
+
+
+np.set_printoptions(precision=3, suppress=True)
+
+# data from file using copy paste function from tutorial
+raw_train_data = get_dataset('sick.data')
+raw_test_data = get_dataset('sick.data')
+
+show_batch(raw_train_data)
+print("-----------------------------")
+
+# DATA PREPROCESSING
+
+SELECT_COLUMNS = ['flags', 'age', 'TSH', 'T3', 'TT4', 'T4U', 'FTI', 'TBG']
+DEFAULTS = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+temp_dataset = get_dataset('sick.data',
+                           select_columns=SELECT_COLUMNS,
+                           column_defaults=DEFAULTS)
+
+show_batch(temp_dataset)
+print(" BATCH END -----------------------------")
+
+packed_dataset = temp_dataset.map(pack)
+
+for features, labels in packed_dataset.take(1):
+    print(features.numpy())
+    print()
+    print(labels.numpy())
+print("-----------------------------")
+
+example_batch, labels_batch = next(iter(temp_dataset))
+
+NUMERIC_FEATURES = ['age', 'TSH', 'T3', 'TT4', 'FTI', 'TBG', 'T4U']
+
+packed_train_data = raw_train_data.map(
+    PackNumericFeatures(NUMERIC_FEATURES))
+
+#packed_test_data = raw_test_data.map(
+#   PackNumericFeatures(NUMERIC_FEATURES))
+
+show_batch(packed_train_data)
+print("-----------------------------")
+
+example_batch, labels_batch = next(iter(packed_train_data))
+desc = pd.read_csv('sick.data')[NUMERIC_FEATURES].describe()
+
+MEAN = np.array(desc.T['mean'])
+STD = np.array(desc.T['std'])
+
+# See what you just created.
+normalizer = functools.partial(normalize_numeric_data, mean=MEAN, std=STD)
+
+numeric_column = tf.feature_column.numeric_column('numeric', normalizer_fn=normalizer, shape=[len(NUMERIC_FEATURES)])
+numeric_columns = [numeric_column]
+
+print(example_batch['numeric'])
+print("-----------------------------")
+numeric_layer = tf.keras.layers.DenseFeatures(numeric_columns)
+print(numeric_layer(example_batch).numpy())
+print("-----------------------------")
 true_or_false = ['f', 't']
-header_cat = {
-        'sex': ['M', 'F'],
-        'on thyroxine': true_or_false,
-        'query on thyroxine': true_or_false,
-        'on antithyroid medication': true_or_false,
-        'sick': true_or_false,
-        'pregnant': true_or_false,
-        'thyroid surgery': true_or_false,
-        'I131 treatment': true_or_false,
-        'query hypothyroid': true_or_false,
-        'lithium': true_or_false,
-        'goitre': true_or_false,
-        'tumor': true_or_false,
-        'hypopituitary': true_or_false,
-        'psych': true_or_false,
-        'TSH measured': true_or_false,
-        'TT4 measured': true_or_false,
-        'T4U measured': true_or_false,
-        'FTI measured': true_or_false,
-        'TBG measured': true_or_false,
-        'referral source': ['WEST', 'STMW', 'SVHC', 'SVI', 'SVHD', 'other']
+CATEGORIES = {
+    'sex': ['M', 'F'],
+    'on thyroxine': true_or_false,
+    'query on thyroxine': true_or_false,
+    'on antithyroid medication': true_or_false,
+    'sick': true_or_false,
+    'pregnant': true_or_false,
+    'thyroid surgery': true_or_false,
+    'I131 treatment': true_or_false,
+    'query hypothyroid': true_or_false,
+    'lithium': true_or_false,
+    'goitre': true_or_false,
+    'tumor': true_or_false,
+    'hypopituitary': true_or_false,
+    'psych': true_or_false,
+    'TSH measured': true_or_false,
+    'TT4 measured': true_or_false,
+    'T4U measured': true_or_false,
+    'FTI measured': true_or_false,
+    'TBG measured': true_or_false,
+    'referral source': ['WEST', 'STMW', 'SVHC', 'SVI', 'SVHD', 'other']
 }
 
+categorical_columns = []
+for feature, vocab in CATEGORIES.items():
+    cat_col = tf.feature_column.categorical_column_with_vocabulary_list(
+        key=feature, vocabulary_list=vocab)
+    categorical_columns.append(tf.feature_column.indicator_column(cat_col))
 
-# batch_size <- parametr
-#
-if __name__ == "__main__":
-    LABEL_COLUMN = 'flags'
-    LABELS = retClassLabels(1)
-    header = getHeader('sick.data')
-    train = get_dataset('sick.data', column_names=header)
+print(categorical_columns)
+print("-----------------------------")
 
-    model = tf.keras.Sequential([
-        tf.keras.layers.Dense(29),
-        tf.keras.layers.Dense(128, activation='relu'),
-        tf.keras.layers.Dense(128, activation='relu'),
-        tf.keras.layers.Dense(1),
-    ])
+categorical_layer = tf.keras.layers.DenseFeatures(categorical_columns)
+print(categorical_layer(example_batch).numpy()[0])
+print("-----------------------------")
 
-    model.compile(
-        loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
-        optimizer='adam',
-        metrics=['accuracy'])
+preprocessing_layer = tf.keras.layers.DenseFeatures(categorical_columns + numeric_columns)
 
-    model.fit(train, epochs=5)
+print(preprocessing_layer(example_batch).numpy()[0])
+print("-----------------------------")
+
+# MODEL
+
+model = tf.keras.Sequential([
+    preprocessing_layer,
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dense(1),
+])
+
+model.compile(
+    loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
+    optimizer='adam',
+    metrics=['accuracy'])
+
+train_data = packed_train_data.shuffle(500)
+test_data = packed_test_data
+
+model.fit(train_data, epochs=20)
+
+test_loss, test_accuracy = model.evaluate(test_data)
+
+print('\n\nTest Loss {}, Test Accuracy {}'.format(test_loss, test_accuracy))
+
+predictions = model.predict(test_data)
+
+# Show some results
+for prediction, survived in zip(predictions[:10], list(test_data)[0][1][:10]):
+    prediction = tf.sigmoid(prediction).numpy()
+    print("Predicted survival: {:.2%}".format(prediction[0]),
+          " | Actual outcome: ",
+          ("SURVIVED" if bool(survived) else "DIED"))
